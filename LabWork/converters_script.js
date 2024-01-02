2f8cc0432be8ec382fec945e20038cd2238705d4
Adam Kim <keinoadamdev@gmail.com>
2024-01-02 18:04:54 +0300

    JS enhancement to converters page

--- LabWork/converters_script.js
+++ LabWork/converters_script.js
@@ -9,3 +9,17 @@ function tempconv() {
     //     return false;
     // }
 }
+function weight(){
+    //To convert KGs to Pounds
+    // KG * 2.2
+    var kg = document.getElementById("kg").value;
+    var lbs = kg * 2.2
+    document.getElementById("lbs").value = lbs
+}
+function distance(){
+    //To convert KMs to Miles
+    // KM * 0.62137
+    var km = document.getElementById("km").value;
+    var m = km * 0.62137
+    document.getElementById("m").value = m
+}
