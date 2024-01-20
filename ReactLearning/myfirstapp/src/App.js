303245b54126c6dd9e9d45dcd3389596364079df
Adam Kim <keinoadamdev@gmail.com>
2024-01-20 14:00:41 +0300

    First react app

---
+++ ReactLearning/myfirstapp/src/App.js
@@ -0,0 +1,12 @@
+function App(props) {
+  const currDate = new Date();
+  return (
+      <div>
+      <h1>Adam Keino is my name</h1>
+      <h2>The date today is {currDate.toLocaleDateString()}</h2>
+      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
+      </div>
+  );
+}
+
+export default App;
