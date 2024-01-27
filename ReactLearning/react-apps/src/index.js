753ef8062c87e34ea44558b02caaacb097079160
Adam Kim <keinoadamdev@gmail.com>
2024-01-27 12:17:57 +0300

    Reducing file sizes

--- ReactLearning/react-apps/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
-
-// This is how it it is done from React 18 onwards
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App color="rgb(20,224,208" size="40px"/>);
-root.render(<App color2="rgb(0,207,209)" size="35px"/>)
